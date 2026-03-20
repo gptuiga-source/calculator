9b7849252c9f4562ae22036abeb3d7706923127c
Tuiga <gptuiga@gmail.com>
2026-03-21 01:57:03 +0300

    update

--- js/test.js
+++ js/test.js
@@ -2,6 +2,8 @@ const input1 = document.getElementById('input1')
 const input2 = document.getElementById('input2')
 const plus = document.getElementById('plus')
 const minus = document.getElementById('minus')
+const divide = document.getElementById('divide')
+const multiply = document.getElementById('multiply')
 const submit = document.getElementById('submit')
 const result = document.getElementById('result')
 let action = null
@@ -12,6 +14,13 @@ plus.onclick = function () {
 
 minus.onclick = function () {
 	action = '-'
+}
+
+divide.onclick = function () {
+	action = '/'
+}
+multiply.onclick = function () {
+	action = '*'
 }
 
 function greenOrRed(answer) {
@@ -28,7 +37,15 @@ function number(ipt1, ipt2, action1) {
 	const num1 = Number(ipt1.value)
 	const num2 = Number(ipt2.value)
 
-	return action1 == '+' ? num1 + num2 : num1 - num2
+	if (action1 == '+') {
+		return num1 + num2
+	} else if (action == '-') {
+		return num1 - num2
+	} else if (action == '/') {
+		return num1 / num2
+	} else {
+		return num1 * num2
+	}
 }
 
 submit.onclick = function () {
